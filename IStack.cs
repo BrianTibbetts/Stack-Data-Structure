993bf5e85c70dddfe0858943628a537989b58f03
BrianTibbetts <50724793+BrianTibbetts@users.noreply.github.com>
2021-07-18 13:28:37 -0400

    IStack
    Created a Stack Interface

---
+++ IStack.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * IStack - Stack functions to be implemented
+ */
+
+using System;
+
+namespace StackDataStructure
+{
+    interface IStack<T>
+    {
+        /**
+         * Returns the number of elements in the stack.
+         * @ return number of elements in the stack
+         * 
+         */
+        int Size();
+
+        /**
+         * Tests whether the stack is empty.
+         * @return true if the stack is empty, false otherwise
+         */
+        Boolean IsEmpty();
+
+        /**
+         * Inserts an element at the top of the stack.
+         * @param e the element to be inserted
+         */
+        void Push(T t);
+
+        /**
+         * Returns, but does not remove, the element at the top of the stack.
+         * @return top element in the stack (or null if empty)
+         */
+        T Top();
+
+        /**
+         * Removes and returns the top element from the stack.
+         * @return element removed (or null if empty)
+         */
+        T Pop();
+    }
+}
